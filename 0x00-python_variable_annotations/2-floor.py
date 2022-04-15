acded68f4b6cfede051eac6f139600c3b73b64c4
Leul Yiheyis <leulykpro@gmail.com>
2022-04-15 19:16:31 +0300

    a type-annotated function to computer floor of a float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+    Variable annotations: task2
+"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+        A type-annotated function computing
+        the floor a floating point number
+    """
+    return math.floor(n)
