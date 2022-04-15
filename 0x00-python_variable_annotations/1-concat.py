fc9daa8ddc3c0853ad2b1d99357e15152f720069
Leul Yiheyis <leulykpro@gmail.com>
2022-04-15 19:10:51 +0300

    a type-annotated string concatenation function

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+    Second type-annotated function
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+        A type-annotated string concatenation
+        function
+    """
+    return str1 + str2
