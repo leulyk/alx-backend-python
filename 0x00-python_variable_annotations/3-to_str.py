468da23cb6045b8b829dc9e7f08243f175fb86e1
Leul Yiheyis <leulykpro@gmail.com>
2022-04-15 19:21:12 +0300

    a type-annotated function to return string representation of a floating point number

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+    Variable annotations: task 3
+"""
+
+
+def to_str(n: float) -> str:
+    """
+        A type-annotated function to
+        return string representation
+        of a floating point number
+    """
+    return str(n)
