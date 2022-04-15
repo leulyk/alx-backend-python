5093bf7ed378e5a45f2b59675175be2c90c85b7b
Leul Yiheyis <leulykpro@gmail.com>
2022-04-15 19:05:50 +0300

    a type-annotated function

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+    Variable annotations exercise
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+        a type annotated function that takes
+        two floats and returns a float
+    """
+    return a + b
