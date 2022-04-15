5e4d22ccee2ad780f7397e90eab40ff4bdc915b5
Leul Yiheyis <leulykpro@gmail.com>
2022-04-15 19:33:13 +0300

    define annotated variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+    Module to demonstrate variable annotations
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
