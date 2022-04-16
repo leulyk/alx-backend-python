75ae25f29d7028a5a0210e8e7aada5882e4e181d
Leul Yiheyis <leulykpro@gmail.com>
2022-04-16 10:45:09 +0300

    get a value safely from a mapping

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+"""
+    Python variable annotations project
+    Task 11
+"""
+
+from typing import Any, Mapping, TypeVar, Union
+
+T = TypeVar('T')
+
+
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[T, None] = None) -> Union[Any, T]:
+    """
+        Demonstrates the Mapping and TypeVar
+    """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
