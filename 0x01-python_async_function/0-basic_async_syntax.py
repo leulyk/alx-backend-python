30866172d3c2944c2b1e8a0b5f05019a219f5349
Leul Yiheyis <leulykpro@gmail.com>
2022-04-18 19:38:35 +0300

    a coroutine that wait for a random delay

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+    Project: Python - Async
+    Task 0
+"""
+
+import asyncio
+from random import uniform
+
+
+async def wait_random(max_delay=10):
+    """
+        A coroutine that waits for a random delay
+        and returns the delay time in seconds
+    """
+    delay = uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
