d11036bc961e0c5a49568652c7a0a5ffa366057d
Leul Yiheyis <leulykpro@gmail.com>
2022-04-19 20:45:09 +0300

    1. Fix code based on requirement that sleep is for only 1 second 2. Change type annotation from AsyncGenerator to Generator even though mypy complains

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,15 +7,14 @@
 
 import asyncio
 from random import uniform
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
         A coroutine that generates a random number between
         0 and 10
     """
     for i in range(10):
-        delay = uniform(0, 10)
-        await asyncio.sleep(delay)
-        yield delay
+        await asyncio.sleep(1)
+        yield uniform(0, 10)
