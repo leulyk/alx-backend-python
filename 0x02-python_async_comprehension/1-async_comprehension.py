56d2c5a457065d8ac6df852dab627af6d14741b5
Leul Yiheyis <leulykpro@gmail.com>
2022-04-19 20:59:29 +0300

    add type annotation to function

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -5,9 +5,11 @@
     Task 1
 """
 
+from typing import List
+
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """ Use async comprehension to return a list from a generator """
     return [i async for i in async_generator()]
