1cf392976e773ed50d5b6ae9b56ed48a8d235da7
Leul Yiheyis <leulykpro@gmail.com>
2025-05-16 16:39:45 +0300

    feat: implement db connection, table creation & data insertion functionality

--- python-generators-0x00/seed.py
+++ python-generators-0x00/seed.py
@@ -1,15 +1,47 @@
+import mysql.connector
+import csv
 
 def connect_db():
-    pass 
+    return mysql.connector .connect(
+        user='root',
+        password='password',
+        host='127.0.0.1'
+    )
 
 def create_database(connection):
-    pass
+    with connection.cursor() as cursor:
+        cursor.execute("CREATE DATABASE {}".format("ALX_prodev"))
 
 def connect_to_prodev():
-    pass
+    return mysql.connector .connect(
+        user='root',
+        password='password',
+        host='127.0.0.1',
+        database='ALX_prodev'
+    )
 
 def create_table(connection):
-    pass
+    with connection.cursor() as cursor:
+        cursor.execute(
+            """
+                CREATE TABLE user_data (
+                    user_id CHAR(36) PRIMARY KEY,
+                    name VARCHAR(255) NOT NULL,
+                    email VARCHAR(255) NOT NULL,
+                    age DECIMAL NOT NULL
+                )
+            """
+        )
 
 def insert_data(connection, data):
-    pass
+    with connection.cursor() as cursor:
+        sql = "INSERT INTO user_data (user_id, name, email, age) VALUES (%s, %s, %s, %s)"
+
+        with open(data, 'r') as file:
+            csv_data = csv.reader(file)
+            next(csv_data) # skip header
+
+            for row in csv_data:
+                cursor.execute(sql, row)
+
+            connection.commit()
