9d91ca44469e445f5351818c1aaa821418c99b44
Leul Yiheyis <leulykpro@gmail.com>
2025-05-16 15:11:56 +0300

    feat: add requirements file, tests and function definition

---
+++ python-generators-0x00/seed.py
@@ -0,0 +1,15 @@
+
+def connect_db():
+    pass 
+
+def create_database(connection):
+    pass
+
+def connect_to_prodev():
+    pass
+
+def create_table(connection):
+    pass
+
+def insert_data(connection, data):
+    pass
