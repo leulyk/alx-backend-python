a25c3826d1f38b9b2f84a897264335910b0ee809
Leul Yiheyis <leulykpro@gmail.com>
2025-05-17 21:15:20 +0300

    refactor: use context manager for handling database connection

--- python-generators-0x00/0-stream_users.py
+++ python-generators-0x00/0-stream_users.py
@@ -3,11 +3,9 @@ import mysql.connector
 seed = __import__('seed')
 
 def stream_users():
-    connection = seed.connect_to_prodev()
-    with connection.cursor(buffered=True, dictionary=True) as cursor:
-        query = "SELECT * FROM user_data"
-        cursor.execute(query)
-        for record in cursor:
-            yield record
-
-    connection.close()
+    with seed.connect_to_prodev() as connection:
+        with connection.cursor(buffered=True, dictionary=True) as cursor:
+            query = "SELECT * FROM user_data"
+            cursor.execute(query)
+            for record in cursor:
+                yield record
