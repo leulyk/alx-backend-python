d0c22cac3fad5e68615c95d0e14461a2e91233ef
Leul Yiheyis <leulykpro@gmail.com>
2025-05-18 11:58:50 +0300

    delegate age filtering to the calling function

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -2,15 +2,18 @@
 seed = __import__('seed')
 
 def batch_processing(batch_size):
-    for user in stream_users_in_batches(batch_size):
-        print(user)
+    for users in stream_users_in_batches(batch_size):
+        for user in users:
+            if user['age'] > 25:
+                print(user)
+    return
 
 def stream_users_in_batches(batch_size):
     with seed.connect_to_prodev() as connection:
         with connection.cursor(buffered=True, dictionary=True) as cursor:
             offset = 0
             while True:
-                query = "SELECT * FROM user_data WHERE age > 25 LIMIT {} OFFSET {}".format(batch_size, offset)
+                query = "SELECT * FROM user_data LIMIT {} OFFSET {}".format(batch_size, offset)
                 cursor.execute(query)
                 rows = cursor.fetchall()
 
