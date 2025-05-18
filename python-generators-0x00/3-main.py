ed0f3ecbb9b5a1f6d23dab526afd7c7a21a007bd
Leul Yiheyis <leulykpro@gmail.com>
2025-05-18 12:10:23 +0300

    feat: implement lazy paginator

---
+++ python-generators-0x00/3-main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+lazy_paginator = __import__('2-lazy_paginate').lazy_paginate
+
+try:
+    for page in lazy_paginator(5):
+        for user in page:
+            print(user)
+
+except BrokenPipeError:
+    sys.stderr.close()
