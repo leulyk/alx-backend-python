4e8fb488749bad75e650582f6694b1c501eab105
Leul Yiheyis <leulykpro@gmail.com>
2025-05-25 11:51:45 +0300

    implementation of a context manager for sqlite database connections

---
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+class DatabaseConnection(object):
+    def __init__(self, db_file):
+        self.db_file = db_file
+        self.connection = None
+        self.cursor = None
+
+    def __enter__(self):
+        self.connection = sqlite3.connect(self.db_file)
+        self.cursor = self.connection.cursor()
+        return self.connection
+
+    def __exit__(self, type, value, traceback):
+        self.connection.close()
+
+with DatabaseConnection('users.db') as connection:
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM users")
+    users = cursor.fetchall()
+    print(users)
