e8fb4c4eab50dffe60c058eab84be159e49f9382
Leul Yiheyis <leulykpro@gmail.com>
2025-05-25 12:34:07 +0300

    implementation of concurrent query execution

---
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -0,0 +1,33 @@
+import asyncio
+import aiosqlite
+
+async def async_fetch_users():
+    async with aiosqlite.connect('users.db') as db:
+        async with db.execute("SELECT * FROM users") as cursor:
+            results = await cursor.fetchall()
+        
+    return results
+        
+
+async def async_fetch_older_users():
+    async with aiosqlite.connect('users.db') as db:
+        async with db.execute("SELECT * FROM users WHERE age > ?", (40,)) as cursor:
+            results = await cursor.fetchall()
+            
+    return results
+
+async def fetch_users():
+    all_users, old_users = await asyncio.gather(
+        async_fetch_users(),
+        async_fetch_older_users()
+    )
+
+    if all_users:
+        for index, user in enumerate(all_users):
+            print(f"{index}: {user}")
+
+    if old_users:
+        for i, user in enumerate(old_users):
+            print(f"{i}: {user}")
+
+asyncio.run(fetch_users())
