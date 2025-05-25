1339cc918670d4290cc96ce3712410c78e10a153
Leul Yiheyis <leulykpro@gmail.com>
2025-05-25 12:16:36 +0300

    refactor database connector

--- python-context-async-perations-0x02/0-databaseconnection.py
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-class DatabaseConnection(object):
+class DatabaseConnection:
     def __init__(self, db_file):
         self.db_file = db_file
         self.connection = None
@@ -9,13 +9,12 @@ class DatabaseConnection(object):
     def __enter__(self):
         self.connection = sqlite3.connect(self.db_file)
         self.cursor = self.connection.cursor()
-        return self.connection
+        return (self.connection, self.cursor)
 
     def __exit__(self, type, value, traceback):
         self.connection.close()
 
-with DatabaseConnection('users.db') as connection:
-    cursor = connection.cursor()
+with DatabaseConnection('users.db') as (connection, cursor):
     cursor.execute("SELECT * FROM users")
     users = cursor.fetchall()
     print(users)
