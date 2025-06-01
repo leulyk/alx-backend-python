ff3dbc1c7a395cd728c0745ef19807a39076ef8c
Leul Yiheyis <leulykpro@gmail.com>
2025-06-01 19:26:08 +0300

    improve serializer and migrate new changes

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -1,17 +1,34 @@
 from rest_framework import serializers
-from models import User, Message, Conversation
+from .models import User, Message, Conversation
 
 class UserSerializer(serializers.ModelSerializer):
+    full_name = serializers.SerializerMethodField()
+    phone_number = serializers.CharField(max_length=15)
+
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ['user_id', 'first_name', 'last_name', 'email', 'phone_number']
+    
+    def validate_phone_number(self, value):
+        if not value.startswith('+'):
+            raise serializers.ValidationError("Phone number must start with '+'")
+        return value
+    
+    def get_full_name(self, obj):
+        return f"{obj.first_name} {obj.last_name}"
 
 class MessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Message
-        fields = '__all__'
+        fields = ['message_id', 'message_body', 'sender', 'sent_at', 'created_at']
+    
 
 class ConversationSerializer(serializers.ModelSerializer):
+    participant_count = serializers.SerializerMethodField()
+
     class Meta:
         model = Conversation
-        fields = '__all__'
+        fields = ['conversation_id', 'participants']
+
+    def get_participan_count(self, obj):
+        return obj.participants.count()
